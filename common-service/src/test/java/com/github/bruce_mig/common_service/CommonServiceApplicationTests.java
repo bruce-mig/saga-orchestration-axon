0127da6ddd77c15ddfc8a7fcb3a83b521965bd6d
bruce-mig <brucetmig@icloud.com>
2025-05-30 16:38:28 +0200

    first commit

---
+++ common-service/src/test/java/com/github/bruce_mig/common_service/CommonServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.bruce_mig.common_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ order-service/src/test/java/com/github/bruce_mig/order_service/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.bruce_mig.order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
