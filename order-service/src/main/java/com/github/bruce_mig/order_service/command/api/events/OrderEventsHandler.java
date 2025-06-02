28dfa7fa2c7bc3690cc0989eb986f34627c9737b
bruce-mig <brucetmig@icloud.com>
2025-06-02 21:51:34 +0200

    chore: added event handler for OrderCompletedEvent

--- order-service/src/main/java/com/github/bruce_mig/order_service/command/api/events/OrderEventsHandler.java
+++ order-service/src/main/java/com/github/bruce_mig/order_service/command/api/events/OrderEventsHandler.java
@@ -1,5 +1,6 @@
 package com.github.bruce_mig.order_service.command.api.events;
 
+import com.github.bruce_mig.commons.events.OrderCompletedEvent;
 import com.github.bruce_mig.order_service.command.api.data.Order;
 import com.github.bruce_mig.order_service.command.api.data.OrderRepository;
 import org.axonframework.eventhandling.EventHandler;
@@ -19,6 +20,14 @@ public class OrderEventsHandler {
     public void on(OrderCreatedEvent event){
         Order order =  new Order();
         BeanUtils.copyProperties(event, order);
+        orderRepository.save(order);
+    }
+
+    @EventHandler
+    public void on(OrderCompletedEvent event){
+        // todo: if present or else throw
+        Order order = orderRepository.findById(event.getOrderId()).get();
+        order.setOrderStatus(event.getOrderStatus());
         orderRepository.save(order);
     }
 }
