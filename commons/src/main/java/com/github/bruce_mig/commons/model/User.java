8ad7374476aed149a052e16e0796279d77510a4c
bruce-mig <brucetmig@icloud.com>
2025-06-02 21:19:08 +0200

    added common models

---
+++ commons/src/main/java/com/github/bruce_mig/commons/model/User.java
@@ -0,0 +1,17 @@
+package com.github.bruce_mig.commons.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private String userId;
+    private String firstName;
+    private String lastName;
+    private CardDetails cardDetails;
+}
