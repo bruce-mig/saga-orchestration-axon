0343a12819e484aba63b7826df7c116b377ac9e8
bruce-mig <brucetmig@icloud.com>
2025-06-02 21:52:28 +0200

    chore: added saga event handlers for  OrderShippedEvent and OrderCompletedEvent

--- order-service/src/main/java/com/github/bruce_mig/order_service/command/api/saga/OrderProcessingSaga.java
+++ order-service/src/main/java/com/github/bruce_mig/order_service/command/api/saga/OrderProcessingSaga.java
@@ -1,15 +1,19 @@
 package com.github.bruce_mig.order_service.command.api.saga;
 
+import com.github.bruce_mig.commons.commands.CompleteOrderCommand;
 import com.github.bruce_mig.commons.commands.ShipOrderCommand;
 import com.github.bruce_mig.commons.commands.ValidatePaymentCommand;
+import com.github.bruce_mig.commons.events.OrderCompletedEvent;
 import com.github.bruce_mig.commons.events.OrderShippedEvent;
 import com.github.bruce_mig.commons.events.PaymentProcessedEvent;
 import com.github.bruce_mig.commons.model.User;
 import com.github.bruce_mig.commons.queries.GetUserPaymentDetailsQuery;
+import com.github.bruce_mig.commons.utils.OrderStatus;
 import com.github.bruce_mig.order_service.command.api.events.OrderCreatedEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.axonframework.messaging.responsetypes.ResponseTypes;
+import org.axonframework.modelling.saga.EndSaga;
 import org.axonframework.modelling.saga.SagaEventHandler;
 import org.axonframework.modelling.saga.StartSaga;
 import org.axonframework.queryhandling.QueryGateway;
@@ -75,6 +79,23 @@ public class OrderProcessingSaga {
 
     @SagaEventHandler(associationProperty = "orderId")
     public void handle(OrderShippedEvent event){
+        log.info("OrderShippedEvent in Saga for OrderId: {}", event.getOrderId());
+
+        CompleteOrderCommand completeOrderCommand  =CompleteOrderCommand.builder()
+                .orderId(event.getOrderId())
+                .orderStatus(OrderStatus.ORDER_APPROVED)
+                .build();
+
+        commandGateway.send(completeOrderCommand);
+    }
+
+    @EndSaga
+    @SagaEventHandler(associationProperty = "orderId")
+    public void handle(OrderCompletedEvent event){
+        log.info("OrderCompletedEvent in Saga for OrderId: {}", event.getOrderId());
+
+        // todo : add notification service
+
 
     }
 
