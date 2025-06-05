23a9e368cc113038fa2c643598bd1d792cff43ee
bruce-mig <brucetmig@icloud.com>
2025-06-05 09:49:51 +0200

    chore: added logs

--- payment-service/src/main/java/com/github/bruce_mig/payment_service/command/api/events/PaymentEventsHandler.java
+++ payment-service/src/main/java/com/github/bruce_mig/payment_service/command/api/events/PaymentEventsHandler.java
@@ -5,12 +5,15 @@ import com.github.bruce_mig.commons.events.PaymentProcessedEvent;
 import com.github.bruce_mig.commons.utils.PaymentStatus;
 import com.github.bruce_mig.payment_service.command.api.data.Payment;
 import com.github.bruce_mig.payment_service.command.api.data.PaymentRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.axonframework.eventhandling.EventHandler;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.util.NoSuchElementException;
 
 @Component
+@Slf4j
 public class PaymentEventsHandler {
 
     private final PaymentRepository paymentRepository;
@@ -21,20 +24,27 @@ public class PaymentEventsHandler {
 
     @EventHandler
     public void on(PaymentProcessedEvent event){
+        String paymentId = event.getPaymentId();
+
         Payment payment = Payment.builder()
-                .paymentId(event.getPaymentId())
+                .paymentId(paymentId)
                 .orderId(event.getOrderId())
                 .paymentStatus(PaymentStatus.PAYMENT_COMPLETED)
                 .time(LocalDateTime.now())
                 .build();
 
         paymentRepository.save(payment);
+        log.info("Handled PaymentProcessed event: {}", event);
     }
 
     @EventHandler
     public void on(PaymentCancelledEvent event){
-        Payment payment = paymentRepository.findById(event.getPaymentId()).get();
+        String paymentId = event.getPaymentId();
+        Payment payment = paymentRepository.findById(paymentId)
+                        .orElseThrow(() -> new NoSuchElementException("Payment not found with id: " + paymentId));
+
         payment.setPaymentStatus(event.getPaymentStatus());
         paymentRepository.save(payment);
+        log.info("Handled PaymentCancelled event: {}", event);
     }
 }
