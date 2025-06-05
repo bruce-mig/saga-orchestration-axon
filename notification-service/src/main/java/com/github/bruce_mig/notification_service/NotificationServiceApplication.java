c6dd7864748975b19d876d244a682e6b2167f572
bruce-mig <brucetmig@icloud.com>
2025-06-05 09:50:35 +0200

    fix: xStream deserialization bug

--- notification-service/src/main/java/com/github/bruce_mig/notification_service/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/github/bruce_mig/notification_service/NotificationServiceApplication.java
@@ -1,9 +1,12 @@
 package com.github.bruce_mig.notification_service;
 
+import com.github.bruce_mig.notification_service.config.AxonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({ AxonConfig.class })
 public class NotificationServiceApplication {
 
 	public static void main(String[] args) {
