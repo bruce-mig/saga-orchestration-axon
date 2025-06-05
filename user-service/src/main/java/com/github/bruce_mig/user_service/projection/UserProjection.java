63d684ed31f1e62e86681035039fea31a0ff52e4
bruce-mig <brucetmig@icloud.com>
2025-06-05 09:49:16 +0200

    chore: fetch user from db

--- user-service/src/main/java/com/github/bruce_mig/user_service/projection/UserProjection.java
+++ user-service/src/main/java/com/github/bruce_mig/user_service/projection/UserProjection.java
@@ -3,27 +3,48 @@ package com.github.bruce_mig.user_service.projection;
 import com.github.bruce_mig.commons.model.CardDetails;
 import com.github.bruce_mig.commons.model.User;
 import com.github.bruce_mig.commons.queries.GetUserPaymentDetailsQuery;
+import com.github.bruce_mig.user_service.data.UserEntity;
+import com.github.bruce_mig.user_service.data.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.axonframework.queryhandling.QueryHandler;
 import org.springframework.stereotype.Component;
 
+import java.util.NoSuchElementException;
+
 @Component
+@Slf4j
 public class UserProjection {
+
+    private final UserRepository userRepository;
+
+    public UserProjection(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @QueryHandler
     public User getUserPaymentDetails(GetUserPaymentDetailsQuery query){
-        // todo: query user details from actual database
-        CardDetails cardDetails = CardDetails.builder()
-                .name("Bruce Migeri")
-                .validUntilMonth(8)
-                .validUntilYear(2027)
-                .cardNumber("123456789")
-                .cvv(111)
-                .build();
-        return User.builder()
-                .userId(query.getUserId())
-                .firstName("Bruce")
-                .lastName("Migeri")
+        String userId = query.getUserId();
+        UserEntity userEntity = userRepository.findById(userId)
+                .orElseThrow(() -> new NoSuchElementException("User not found with id: " + userId));
+
+        CardDetails cardDetails = null;
+        if (userEntity.getCardDetails() != null) {
+            cardDetails = CardDetails.builder()
+                    .name(userEntity.getCardDetails().getName())
+                    .cardNumber(userEntity.getCardDetails().getCardNumber())
+                    .validUntilMonth(userEntity.getCardDetails().getValidUntilMonth())
+                    .validUntilYear(userEntity.getCardDetails().getValidUntilYear())
+                    .cvv(userEntity.getCardDetails().getCvv())
+                    .build();
+        }
+        User user = User.builder()
+                .userId(userEntity.getUserId())
+                .firstName(userEntity.getFirstName())
+                .lastName(userEntity.getLastName())
+                .email(userEntity.getEmail())
                 .cardDetails(cardDetails)
                 .build();
+        log.info("Handled GetUserPaymentDetailsQuery for user with id: {}", userId );
+        return user;
     }
 }
