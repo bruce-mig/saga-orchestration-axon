518c80db03dd833a8f9cdd7d2ad9a2e74b6b1894
bruce-mig <brucetmig@icloud.com>
2025-06-05 09:49:29 +0200

    chore: added logs

--- shipment-service/src/main/java/com/github/bruce_mig/shipment_service/command/api/events/ShipmentEventsHandler.java
+++ shipment-service/src/main/java/com/github/bruce_mig/shipment_service/command/api/events/ShipmentEventsHandler.java
@@ -3,11 +3,13 @@ package com.github.bruce_mig.shipment_service.command.api.events;
 import com.github.bruce_mig.commons.events.OrderShippedEvent;
 import com.github.bruce_mig.shipment_service.command.api.data.Shipment;
 import com.github.bruce_mig.shipment_service.command.api.data.ShipmentRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.axonframework.eventhandling.EventHandler;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class ShipmentEventsHandler {
 
     private final ShipmentRepository shipmentRepository;
@@ -21,5 +23,6 @@ public class ShipmentEventsHandler {
         Shipment shipment = new Shipment();
         BeanUtils.copyProperties(event, shipment);
         shipmentRepository.save(shipment);
+        log.info("Handled OrderShippedEvent event: {}", event);
     }
 }
